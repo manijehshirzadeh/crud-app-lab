72fdca2c8976c8501be20ab36b5d6676932d9aa3
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-06-19 17:34:43 +1000

    Establish database connection

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const server = express();
 
-server.get("/", async (req, res) => {
-  res.send("hello, friend!");
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 server.get("/test", async (req, res) => {
